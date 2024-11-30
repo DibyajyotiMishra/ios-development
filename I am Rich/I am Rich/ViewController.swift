cbb184b240785098e1f06412f6da9f671e4a765e
dibyajyotimishra <dibyajyotimishra14@gmail.com>
2024-12-01 01:36:04 +0530

    feat: building first ios app - I am Rich

---
+++ I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Dibyajyoti Mishra on 01/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
