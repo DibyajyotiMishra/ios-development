76d15d0d3560ff0c3a4a7526f5a95c91ed378886
dibyajyotimishra <dibyajyotimishra14@gmail.com>
2024-12-05 10:27:37 +0530

    feat: completed dicee

--- Dicee/Dicee/ViewController.swift
+++ Dicee/Dicee/ViewController.swift
@@ -12,13 +12,20 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        diceImageView1.image = UIImage(imageLiteralResourceName: "DiceSix")
-        diceImageView2.image = UIImage(imageLiteralResourceName: "DiceThree")
     }
 
-
+    @IBAction func rollButtonPressed(_ sender: Any) {
+        let diceImages = [UIImage(imageLiteralResourceName: "DiceOne"), UIImage(imageLiteralResourceName: "DiceTwo"), UIImage(imageLiteralResourceName: "DiceThree"), UIImage(imageLiteralResourceName: "DiceFour"), UIImage(imageLiteralResourceName: "DiceFive"), UIImage(imageLiteralResourceName: "DiceSix") ]
+        
+        var leftDiceNumber = Int.random(in: 0...5)
+        var rightDiceNumber = Int.random(in: 0...5)
+        
+        diceImageView1.image = diceImages[leftDiceNumber]
+        diceImageView2.image = diceImages[rightDiceNumber]
+    }
+    
 }
 
